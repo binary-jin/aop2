cda07c06a03387908a63c6f71ed3a5186294bfb0
박이진 <yi-jin@bag-ijin-ui-MacBookAir.local>
2022-09-23 18:15:25 +0900

    Initial commit

---
+++ src/main/java/com/example/aop2/controller/RestApiController.java
@@ -0,0 +1,27 @@
+package com.example.aop2.controller;
+
+
+import com.example.aop2.dto.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class RestApiController {
+
+    @GetMapping("/get/{id}")
+    public String get(@PathVariable Long id, @RequestParam String name){
+        System.out.println("get method");
+        System.out.println("get method :" +id);
+        System.out.println("get method :" +name);
+
+        return id+ " " + name;
+    }
+
+    @PostMapping("/post")
+    public User post(@RequestBody User user){
+        System.out.println("post method :" +user);
+
+        return user;
+    }
+
+}
