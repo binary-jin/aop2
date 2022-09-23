b3bd720e356133da9b965ce8d37eb1248dd6e1f8
yi-jin <dlwls4338@naver.com>
2022-09-23 19:31:16 +0900

    aop(1)

--- src/main/java/com/example/aop2/Aop2Application.java
+++ src/main/java/com/example/aop2/Aop2Application.java
@@ -3,11 +3,15 @@ package com.example.aop2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Base64;
+
 @SpringBootApplication
 public class Aop2Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Aop2Application.class, args);
+        System.out.println(Base64.getEncoder().encodeToString("steve@naver.com".getBytes()));
     }
 
 }
--- src/main/java/com/example/aop2/controller/RestApiController.java
+++ src/main/java/com/example/aop2/controller/RestApiController.java
@@ -1,7 +1,10 @@
 package com.example.aop2.controller;
 
 
+import com.example.aop2.annotation.Decode;
+import com.example.aop2.annotation.Timer;
 import com.example.aop2.dto.User;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,6 +23,28 @@ public class RestApiController {
     @PostMapping("/post")
     public User post(@RequestBody User user){
         System.out.println("post method :" +user);
+
+        return user;
+    }
+
+    @Timer
+    @DeleteMapping("/delete")
+    public void delete() throws InterruptedException {
+
+//        StopWatch stopWatch = new StopWatch(); -->이런 것들을 반복되니까 aop로 빼는 것
+//        stopWatch.start();
+
+        //db logic
+        Thread.sleep(1000*2);
+
+//        stopWatch.stop();
+//        System.out.println("total time:"+stopWatch.getTotalTimeSeconds());
+    }
+
+    @Decode
+    @PutMapping("/put")
+    public User put(@RequestBody User user){
+        System.out.println("put method :" +user);
 
         return user;
     }
